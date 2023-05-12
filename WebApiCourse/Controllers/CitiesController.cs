cc3229625d119fedc8fb474b60661c41b0052b70
Wes.Matlock <wes.matlock@egov.com>
2023-05-12 08:33:03 -0500

    Add dummy data

--- WebApiCourse/Controllers/CitiesController.cs
+++ WebApiCourse/Controllers/CitiesController.cs
@@ -4,6 +4,14 @@ namespace WebApiCourse.Controllers
 {
     public class CitiesController : ControllerBase
     {
-
+        public JsonResult GetCities()
+        {
+           return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, name = "Tom" },
+                    new { id = 2, name = "Jim"}
+                });
+        }
     }
 }
