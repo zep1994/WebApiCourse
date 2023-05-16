fc70008cd26bd37322aa1650e0636d9ba24ab671
Wes.Matlock <wes.matlock@egov.com>
2023-05-16 12:32:29 -0500

    Add Model

---
+++ WebApiCourse/CitiesDataStore.cs
@@ -0,0 +1,28 @@
+﻿using WebApiCourse.Models;
+
+namespace WebApiCourse
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore() 
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Test",
+                    Description = "Test"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Test",
+                    Description = "Test"
+                }
+            };
+        }
+    }
+}
